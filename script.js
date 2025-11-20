504559a9479e525bb80e9f02c07e21cbb789f22b
rochedo55 <patodoido8@gmail.com>
2025-11-20 00:02:50 -0300

    Novo arquivo de script

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let dados = []
 
 async function carregarDados(){
-    let resposta = await fetch("data.json")
+    const resposta = await fetch("data.json")
     dados = await resposta.json()
-    popularLista()
+    popularLista(dados)
 }
 
-function popularLista(){
+function popularLista(dadosParaExibir){
     const secao = document.querySelector("main section")
     secao.innerHTML = "" // Limpa a seção antes de popular
 
-    for(const item of dados){
+    for(const item of dadosParaExibir){
         const article = document.createElement('article')
         article.classList.add('card')
 
@@ -19,9 +19,28 @@ function popularLista(){
             <p>${item.descricao}</p>
             <a href="${item.link}" target="_blank">Saiba mais</a>
         `
-
         secao.appendChild(article)
     }
+}
+
+function iniciarBusca(){
+    const termoBusca = document.getElementById('busca').value.toLowerCase()
+    
+    if (termoBusca === '') {
+        alert("Por favor, digite algo para buscar.")
+        return
+    }
+
+    const resultadoBusca = dados.filter(item => 
+        item.nome.toLowerCase().includes(termoBusca)
+    )
+
+    popularLista(resultadoBusca)
+}
+
+function limparBusca() {
+    document.getElementById('busca').value = ""
+    popularLista(dados)
 }
 
 // Inicia o carregamento dos dados assim que a página é carregada
